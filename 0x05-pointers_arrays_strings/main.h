a87ccfe0697a8ef809172fcce57c66838dc863c3
sedziah <sedziah@gmail.com>
2022-09-19 17:22:29 -0700

    task 0 updated

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _atoi(char *s);
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -10,3 +13,5 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src
+
+#endif
